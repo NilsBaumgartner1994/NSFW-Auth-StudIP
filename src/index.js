210c62c9be0689c38e9407f564a2714e4009c926
Nils <nilsbaumgartner1994@gmail.com>
2021-01-28 09:06:53 +0100

    updated export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // this should be the entry point to your library
 module.exports = {
-    ServerProxy: require('./AuthStudIP').default, // eslint-disable-line global-require
+    AuthStudIP: require('./AuthStudIP').default, // eslint-disable-line global-require
 };
